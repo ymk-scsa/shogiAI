abb393b28ea3f280fc8c3f62bd42638268a9e7ae
kotetsu-nakashima <kotetsu.owl2@gmail.com>
2025-03-30 18:16:57 +0900

    Add activation_function

--- app/domain/activation_function.py
+++ app/domain/activation_function.py
@@ -3,16 +3,28 @@ import torch.nn.functional as F
 
 
 def soft_sin(x: torch.Tensor) -> torch.Tensor:
-    return x / (1 + abs(x))
+    return 2 * x / (1 + abs(x))
+
+
+def scaled_arc_tahn(x: torch.Tensor) -> torch.Tensor:
+    return 2 * x / (1 + abs(x))
+
+
+def quafratic(x: torch.Tensor) -> torch.Tensor:
+    return x * (2 - x)
 
 
 ACTIVATION_FUNCTION_MODE = [
     ACTIVATION_FUNCTION_RELU,
     ACTIVATION_FUNCTION_SOFT_SIN,
-] = range(2)
+    ACTIVATION_FUNCTION_SCALED_ARC_TAHN,
+    ACTIVATION_FUNCTION_QUADRATIC,
+] = range(4)
 
 
 ACTIVATION_FUNCTION_SETTING = {
     ACTIVATION_FUNCTION_RELU: F.relu,
     ACTIVATION_FUNCTION_SOFT_SIN: soft_sin,
+    ACTIVATION_FUNCTION_SCALED_ARC_TAHN: scaled_arc_tahn,
+    ACTIVATION_FUNCTION_QUADRATIC: quafratic,
 }
